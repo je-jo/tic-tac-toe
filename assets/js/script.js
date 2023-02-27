cfc6c39321194224e357b7988dc38d1854f390ea
Jelena Jovicic <jejovicic@gmail.com>
2023-02-27 11:44:56 +0100

    working win conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,17 @@
 const gameBoard = (() => {
     const board = [];
     const Cell = (marker, index) => ({ marker, index });
-    for (let i = 0; i <= 8; i += 1) {
-        // eslint no-plus-plus
+    for (let i = 0; i <= 8; i += 1) { // eslint no-plus-plus
         const cell = Cell("", i);
         board.push(cell);
     }
-    const clearMarkers = () => {
+    const clearBoard = () => {
         board.forEach((cell) => {
             const clearedCell = cell; // eslint no-param-reasign
             clearedCell.marker = "";
         });
     };
-    return { board, clearMarkers };
+    return { board, clearBoard };
 })();
 
 // player objects factory
@@ -26,57 +25,63 @@ const Player = (name, marker, color) => {
     const getColor = () => color;
     return { getName, getMarker, getColor };
 };
-const player1 = Player("Player 1", "X", "hotpink");
-const player2 = Player("Player 2", "O", "blue");
+const player1 = Player("Player 1", "X", "hsl(331, 74%, 67%)");
+const player2 = Player("Player 2", "O", "hsl(195, 95%, 48%)");
 players.push(player1, player2);
 
 // gameplay object
 
 const game = (() => {
-    let activePlayer = players[1];
-
     // switch active player
+    let activePlayer = players[1];
     const switchActivePlayer = () => {
         activePlayer = activePlayer === players[1] ? players[0] : players[1];
         return activePlayer;
     };
 
-    // allow active player to place marker on empty cell
+    // allow player to place marker on empty cell
     const placeMarker = (cell, player) => {
         const activeCell = cell;
         if (!activeCell.marker) { // check if cell is empty
             activeCell.marker = player.getMarker();
         }
     };
-
     // check for gameover
-    const endGame = () => {
-        gameBoard.clearMarkers();
-    };
-    
+
+    // 1. define winning combinations (index of cells)
+    const winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    // 2. transform indexes to placed markers
+    const potentialWins = () => winCombos.map((arr) => arr.map((index) => gameBoard.board[index].marker));
+    // 3. Define winning conditions
+    // 3a. Write a function to return a winning array index - where all cells in array are marked, and all are equal
+    const isThreeInARowIndex = () => potentialWins().findIndex((arr) => arr.every((elem) => (elem) && (elem === arr[0])));
+    // 3b. If there's no winning array, check if all cells are full - a tie
+    const isBoardFull = () => gameBoard.board.every((elem) => elem.marker);
+    // 4. On every cell click, run a function to check if any of the winning conditions are true
     const isGameOver = () => {
-        const winCombos = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        const potentialWins = winCombos.map((arr) =>
-            arr.map((elem) => gameBoard.board[elem].marker)
-        );
-        const isThreeInARow = () => potentialWins.some((arr) => arr.every((elem) => elem === "X"));
-        const isBoardFull = () => gameBoard.board.every((elem) => elem.marker);
-        if (isThreeInARow()) {
-            endGame();
-            console.log("Somebody Won!")
-            return "Somebody Won!"
+        if (isThreeInARowIndex() !== -1) {
+            // 5. If there is a winning array, extract the following from it:
+            // 5a. the winning marker to get to the winning player
+            const winningMarker = potentialWins()[isThreeInARowIndex()][0];
+            // 5b. the winning player
+            const winner = players.find(player => player.getMarker() === winningMarker);
+            // 5c. and the indexes of winning cells for styling
+            /* const winningArray = winCombos[isThreeInARowIndex()];
+            console.log(winningArray) */
+            // 6. clear the game board and return winning message
+            gameBoard.clearBoard();
+            return `${winner.getName()} Won!`
         } if (isBoardFull()) {
-            endGame();
-            console.log("It's a tie!")
+            gameBoard.clearBoard();
             return "It's a tie!"
         }
         return false
@@ -84,23 +89,30 @@ const game = (() => {
     return { switchActivePlayer, placeMarker, isGameOver };
 })();
 
-// function to render gameboard object
+// module for display controller with function to render gameboard object
 
 const displayController = (() => {
+
     const boardDisplay = document.getElementById("board");
+    const clearBoardDisplay = () => {
+        while (boardDisplay.firstChild) {
+            boardDisplay.removeChild(boardDisplay.lastChild);
+        }
+    }
+
     const displayEndGameDialog = document.getElementById("endgame");
     const closeMessage = document.getElementById("endgame-message");
     const closeBtn = document.getElementById("btn-close");
+
     closeBtn.addEventListener("click", () => {
         displayEndGameDialog.close();
     });
+    
 
-    displayEndGameDialog.addEventListener("close", () => {
-        boardDisplay.textContent = "";
-    });
     const renderBoard = () => {
         for (let i = 0; i <= 8; i += 1) {
             const btn = document.createElement("button");
+            btn.textContent = "\u200B"; // to preserve height when there's no marker.
             boardDisplay.appendChild(btn);
         }
         const allButtons = [...boardDisplay.querySelectorAll("button")];
@@ -111,8 +123,9 @@ const displayController = (() => {
             game.placeMarker(gameBoard.board[activeIndex], activePlayer);
             e.currentTarget.style.color = activePlayer.getColor();
             e.currentTarget.textContent = activePlayer.getMarker();
-            if (game.isGameOver()) {
-                closeMessage.textContent = game.isGameOver();
+            const gameOverMessage = game.isGameOver();
+            if (gameOverMessage) {
+                closeMessage.textContent = gameOverMessage;
                 displayEndGameDialog.showModal();
             }
         }
@@ -122,8 +135,15 @@ const displayController = (() => {
         );
     };
 
+    displayEndGameDialog.addEventListener("close", () => {
+        clearBoardDisplay();
+        renderBoard();
+    });
+
     return { renderBoard };
 })();
 
-const btnStart = document.getElementById("start");
-btnStart.addEventListener("click", displayController.renderBoard);
+displayController.renderBoard();
+
+/* const btnStart = document.getElementById("start");
+btnStart.addEventListener("click", displayController.renderBoard); */
